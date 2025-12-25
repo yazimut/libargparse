445491566106f091b6706560da09c726bd0af206
Eugene Azimut <y.azimut@mail.ru>
2025-12-26 01:19:48 +0300

    One more iteration

---
+++ include/argparse/api.hpp
@@ -0,0 +1,52 @@
+/**
+ * @file api.hpp
+ * @author Eugene Azimut (y.azimut@mail.ru)
+ * @brief Declration of library API import/export
+ * @version 1.0.0
+ * @date 2025-12-25
+ *
+ * @copyright Copyright (c) Eugene Azimut, 2025
+ *
+ */
+#pragma once
+
+#if !defined(__cplusplus)
+    #error "Required language is C++"
+#endif
+
+#if __cplusplus < 201103L
+    #error "Required C++ standard is C++11 and higher"
+#endif
+
+#if defined(ARGPARSE_SHARED)
+    #if defined(_WIN32)
+        // Microsoft Windows
+        #if defined(ARGPARSE_EXPORTS)
+            #define ARGPARSE_API __declspec(dllexport)
+        #else
+            #define ARGPARSE_API __declspec(dllimport)
+        #endif
+
+        #define ARGPARSE_HIDDEN
+    #else
+        // Linux,
+        // MacOS (OS X),
+        // Android,
+        // iOS,
+        // Unix-like
+        #if defined(ARGPARSE_EXPORTS)
+            #define ARGPARSE_API __attribute__((visibility("default")))
+        #else
+            #define ARGPARSE_API
+        #endif
+
+        #define ARGPARSE_HIDDEN __attribute__((visibility("hidden")))
+    #endif
+
+    #define ARGPARSE_DEPRECATED(msg) __declspec(deprecated(msg))
+#else
+    #define ARGPARSE_API
+    #define ARGPARSE_HIDDEN
+
+    #define ARGPARSE_DEPRECATED(msg) __attribute__((deprecated(msg)))
+#endif
