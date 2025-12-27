62ab6e5d717fe19c17ddbf5720bc2f6d4ce1c858
Eugene Azimut <y.azimut@mail.ru>
2025-12-27 04:02:05 +0300

    tmp

--- include/argparse/api.hpp
+++ include/argparse/api.hpp
@@ -1,7 +1,7 @@
 /**
  * @file api.hpp
  * @author Eugene Azimut (y.azimut@mail.ru)
- * @brief Declration of library API import/export
+ * @brief Declration of library API export/import
  * @version 1.0.0
  * @date 2025-12-25
  *
@@ -18,35 +18,46 @@
     #error "Required C++ standard is C++11 and higher"
 #endif
 
-#if defined(ARGPARSE_SHARED)
-    #if defined(_WIN32)
-        // Microsoft Windows
+#if defined(_WIN32)
+    // Microsoft Windows
+    #if defined(ARGPARSE_STATIC)
+        #define ARGPARSE_API
+    #else
+        #if !defined(ARGPARSE_SHARED)
+            #define ARGPARSE_SHARED
+        #endif
+
         #if defined(ARGPARSE_EXPORTS)
             #define ARGPARSE_API __declspec(dllexport)
         #else
             #define ARGPARSE_API __declspec(dllimport)
         #endif
-
-        #define ARGPARSE_HIDDEN
-    #else
-        // Linux,
-        // MacOS (OS X),
-        // Android,
-        // iOS,
-        // Unix-like
-        #if defined(ARGPARSE_EXPORTS)
-            #define ARGPARSE_API __attribute__((visibility("default")))
-        #else
-            #define ARGPARSE_API
-        #endif
-
-        #define ARGPARSE_HIDDEN __attribute__((visibility("hidden")))
     #endif
 
+    #define ARGPARSE_HIDDEN
     #define ARGPARSE_DEPRECATED(msg) __declspec(deprecated(msg))
 #else
-    #define ARGPARSE_API
-    #define ARGPARSE_HIDDEN
+    // Linux,
+    // MacOS (OS X),
+    // Android,
+    // iOS,
+    // Unix-like
+    #if defined(ARGPARSE_STATIC)
+        #define ARGPARSE_API
+        #define ARGPARSE_HIDDEN
+    #else
+        #if !defined(ARGPARSE_SHARED)
+            #define ARGPARSE_SHARED
+        #endif
+
+        #if defined(ARGPARSE_EXPORTS)
+            #define ARGPARSE_API __attribute__((visibility("default")))
+            #define ARGPARSE_HIDDEN __attribute__((visibility("hidden")))
+        #else
+            #define ARGPARSE_API
+            #define ARGPARSE_HIDDEN
+        #endif
+    #endif
 
     #define ARGPARSE_DEPRECATED(msg) __attribute__((deprecated(msg)))
 #endif
